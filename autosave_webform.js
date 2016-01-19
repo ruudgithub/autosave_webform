5f7de3d7146aa278761d4cabe17e99aa80f77ec3
Jaap Jansma <jaap@edeveloper.nl>
2016-01-19 16:31:57 +0100

    changed indention and added doc block to meet drupal style

--- autosave_webform.js
+++ autosave_webform.js
@@ -1,143 +1,142 @@
 /**
  * @file
- * This javascript provides the behaviour for autosaving webforms
+ * This javascript provides the behaviour for autosaving webforms.
  *
- * The file consist of two parts the first part attach the behaviour to drupal forms
- * The second part is the class and functionality for saving
+ * The file consist of two parts the first part attach the behaviour to drupal forms.
+ * The second part is the class and functionality for saving.
  */
 
 (function ($) {
 
-    Drupal.behaviors.autosaveWebform = {
-        attach: function (context, settings) {
-            if (Drupal.settings.autosave_webform.form_id) {
-                jQuery('#' + Drupal.settings.autosave_webform.form_id).autosave_webform({
-                    timeout: Drupal.settings.autosave_webform.timeout,
-                    autosave_button: Drupal.settings.autosave_webform.autosave_button
-                });
-            }
-        }
-    };
+  Drupal.behaviors.autosaveWebform = {
+    attach: function (context, settings) {
+      if (Drupal.settings.autosave_webform.form_id) {
+        jQuery('#' + Drupal.settings.autosave_webform.form_id).autosave_webform({
+          timeout: Drupal.settings.autosave_webform.timeout,
+          autosave_button: Drupal.settings.autosave_webform.autosave_button
+        });
+      }
+    }
+  };
 })(jQuery);
 
 function autosave_webform_hide_save_message() {
+  setTimeout(function () {
     console.log("hide");
-    setTimeout(function () {
-        console.log("hide");
-        $(this).css("display", "none");
-    }, 5000);
+    $(this).css("display", "none");
+  }, 5000);
 }
 
 (function ($) {
 
-    $.autosave_webform = function () {
-        return $autosave_webform.getInstance();
+  $.autosave_webform = function () {
+    return $autosave_webform.getInstance();
+  };
+
+  $.fn.autosave_webform = function (options) {
+    var autosave_webform = $autosave_webform.getInstance();
+    autosave_webform.setOptions(options);
+    autosave_webform.start(this);
+    return autosave_webform;
+  };
+
+  $.fn.autosave_webform_show_message = function () {
+    var el = $(this);
+    el.css("display", "");
+    setTimeout(function () {
+      el.css("display", "none");
+    }, 3000);
+  };
+
+  $autosave_webform = (function () {
+    var params = {
+      instantiated: null,
+      started: null
     };
 
-    $.fn.autosave_webform = function (options) {
-        var autosave_webform = $autosave_webform.getInstance();
-        autosave_webform.setOptions(options);
-        autosave_webform.start(this);
-        return autosave_webform;
-    };
+    function init() {
 
-    $.fn.autosave_webform_show_message = function () {
-        var el = $(this);
-        el.css("display", "");
-        setTimeout(function () {
-            el.css("display", "none");
-        }, 3000);
-    };
-
-    $autosave_webform = (function () {
-        var params = {
-            instantiated: null,
-            started: null
-        };
-
-        function init() {
-
-            return {
-                /**
-                 * Set plugin initial options
-                 *
-                 * @param [Object] options
-                 *
-                 * @return void
-                 */
-                setInitialOptions: function (options) {
-                    var defaults = {
-                        timeout: 15,
-                        name: null,
-                        autosave_button: 'edit-autosave',
-                        onSave: function () {
-                        },
-                    };
-                    this.options = this.options || $.extend(defaults, options);
-                },
-
-                /**
-                 * Set plugin options
-                 *
-                 * @param [Object] options
-                 *
-                 * @return void
-                 */
-                setOptions: function (options) {
-                    this.options = this.options || this.setInitialOptions(options);
-                    this.options = $.extend(this.options, options);
-                },
-
-                start: function (targets) {
-                    targets = targets || {};
-                    var self = this;
-                    this.targets = this.targets || [];
-                    this.targets = $.merge(this.targets, targets);
-                    this.targets = $.unique(this.targets);
-                    this.targets = $(this.targets);
-                    if (!params.started) {
-                        params.started = true;
-                        self.saveDataByTimeout();
-                    }
-                },
-
-                saveAllData: function () {
-                    var self = this;
-                    $(self.options.autosave_button).mousedown();
-
-                    if ($.isFunction(self.options.onSave)) {
-                        self.options.onSave.call();
-                    }
-                    self.saveDataByTimeout();
-                },
-
-                saveDataByTimeout: function () {
-                    var self = this;
-                    setTimeout((function () {
-                        function timeout() {
-                            self.saveAllData();
-                        }
-
-                        return timeout;
-                    })(), self.options.timeout * 1000);
-                },
-            };
-        }
-
-        return {
-            getInstance: function () {
-                if (!params.instantiated) {
-                    params.instantiated = init();
-                    params.instantiated.setInitialOptions();
-                }
-                return params.instantiated;
+      return {
+        /**
+         * Set plugin initial options
+         *
+         * @param [Object] options
+         *
+         * @return void
+         */
+        setInitialOptions: function (options) {
+          var defaults = {
+            timeout: 15,
+            name: null,
+            autosave_button: 'edit-autosave',
+            onSave: function () {
             },
+          };
+          this.options = this.options || $.extend(defaults, options);
+        },
 
-            free: function () {
-                params = {};
-                return null;
+        /**
+         * Set plugin options
+         *
+         * @param [Object] options
+         *
+         * @return void
+         */
+        setOptions: function (options) {
+          this.options = this.options || this.setInitialOptions(options);
+          this.options = $.extend(this.options, options);
+        },
+
+        start: function (targets) {
+          targets = targets || {};
+          var self = this;
+          this.targets = this.targets || [];
+          this.targets = $.merge(this.targets, targets);
+          this.targets = $.unique(this.targets);
+          this.targets = $(this.targets);
+          if (!params.started) {
+            params.started = true;
+            self.saveDataByTimeout();
+          }
+        },
+
+        saveAllData: function () {
+          var self = this;
+          $(self.options.autosave_button).mousedown();
+
+          if ($.isFunction(self.options.onSave)) {
+            self.options.onSave.call();
+          }
+          self.saveDataByTimeout();
+        },
+
+        saveDataByTimeout: function () {
+          var self = this;
+          setTimeout((function () {
+            function timeout() {
+              self.saveAllData();
             }
-        };
-    })();
+
+            return timeout;
+          })(), self.options.timeout * 1000);
+        },
+      };
+    }
+
+    return {
+      getInstance: function () {
+        if (!params.instantiated) {
+          params.instantiated = init();
+          params.instantiated.setInitialOptions();
+        }
+        return params.instantiated;
+      },
+
+      free: function () {
+        params = {};
+        return null;
+      }
+    };
+  })();
 
 })(jQuery);
