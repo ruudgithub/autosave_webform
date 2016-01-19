903ec280bfd670f21667437fb6da8bfecede03db
Jaap Jansma <jaap@edeveloper.nl>
2016-01-19 17:32:11 +0100

    changed indention and added doc block to meet drupal style

--- autosave_webform.api.php
+++ autosave_webform.api.php
@@ -1,25 +1,31 @@
 <?php
 /**
  * @file
- *
  * This file contains the api documentation and example of the module autosave_webform.
  *
  * @author Jaap Jansma (CiviCooP) <jaap.jansma@civicoop.org>
+ *
  * @license http://www.gnu.org/licenses/agpl-3.0.html
  */
 
 /**
  * Alter the list of forms configured for autosaving.
  *
- * To enable alter saving at a form add the form_id to the array of $form;
+ * To enable alter saving at a form add the form_id to the array of $form.
  *
- * @param $forms array
- * @param $form
- * @param $form_id
- * @param $form_state
+ * @param array $forms
+ *    The array with current defined forms.
+ * @param array $form
+ *    The form.
+ * @param string $form_id
+ *    The form id.
+ * @param array $form_state
+ *    The form state.
+ *
  * @return array
+ *   Array containing valid form ids for autosaving.
  */
-function hook_autosavewebform_alter_autosaving_forms($forms, $form, $form_id, $form_state) {
+function hook_autosavewebform_alter_autosaving_forms(array $forms, array $form, $form_id, array $form_state) {
     $forms['webform_client_form_1'] = array(
         'no-submit-handler' => false, //set this to true when you don't want a clear saved data on the submitting of your form
         'auto-restore' => true,   //set this to false if you don't want to autoload stored data on your form
@@ -30,14 +36,18 @@ function hook_autosavewebform_alter_autosaving_forms($forms, $form, $form_id, $f
 /**
  * Alter the form_state and form after a restore has taken place.
  *
- * The $data contains the restored data and can be used to alter the $form_state
+ * The $data contains the restored data and can be used to alter the $form_state.
  *
- * @param $form
- * @param $form_id
- * @param $form_state
- * @param $data
+ * @param array $form
+ *    The form.
+ * @param string $form_id
+ *    The form id.
+ * @param array $form_state
+ *    The form state.
+ * @param array $data
+ *    The saved data.
  */
-function hook_autosavewebform_restore_alter(&$form, $form_id, &$form_state, $data) {
+function hook_autosavewebform_restore_alter(array &$form, $form_id, array &$form_state, array $data) {
     if (isset($form_state['webform'])) {
         if (isset($data['values'])) {
             $form_state['values'] = $data['values'];
@@ -52,14 +62,18 @@ function hook_autosavewebform_restore_alter(&$form, $form_id, &$form_state, $dat
 }
 
 /**
- * Alter the data before saving it
+ * Alter the data before saving it.
  *
  * You can use this hook to add additional data for storing.
  *
- * @param $form
- * @param $form_id
- * @param $form_state
- * @param $data
+ * @param array $form
+ *    The form.
+ * @param string $form_id
+ *    The form id.
+ * @param array $form_state
+ *    The form state.
+ * @param array $data
+ *    The data to be saved.
  */
 function hook_autosavewebform_save_alter(&$form, $form_id, &$form_state, &$data) {
     if (isset($form_state['webform'])) {
